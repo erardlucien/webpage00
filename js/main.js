341b8a17877a109d07e1bc3c063d9587481d4ea2
@adf749 <lucien.erard@haw-hamburg.de>
2023-01-18 03:45:50 +0100

    when a link is cliked, the hamburge-menu get closed.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
 'use strict';
 
+let topNav = document.querySelector('.top-nav');
+let hamburgerChecker = document.querySelector('.hamburgerChecker');
+let hamburgerIcon = document.querySelector('.hamburger-icon');
 let navLinks = document.querySelectorAll('.navLink');
 let linkedContents = document.querySelectorAll('.linkedContent');
 let services = document.querySelectorAll('.service');
-let status = ['reduced', 'expanded'];
+let state = ['false', 'true'];
+let dataStatus = ['reduced', 'expanded'];
 let desktopView = window.matchMedia('(min-width: 40em)');
 let goTop = document.querySelector('.gotop');
 let topMainPage = document.querySelector('#top-main-page');
 
+hamburgerIcon.addEventListener('click', () => {
+    
+    if(topNav.getAttribute('data-opened') === state[0])
+    {
+        topNav.setAttribute('data-opened', state[1]);
+        return;
+    }
+
+    if(topNav.getAttribute('data-opened') === state[1])
+    {
+        topNav.setAttribute('data-opened', state[0]);
+    }
+});
 
 for(let index = 0; index < navLinks.length; ++index)
 {
     navLinks[index].addEventListener('click', (event) => {
         event.preventDefault();
+
+        if(topNav.getAttribute('data-opened') === state[1])
+        {
+            topNav.setAttribute('data-opened', state[0]);
+            hamburgerChecker.checked = false;
+        }
 
         scrollTo(
             {
@@ -26,10 +49,10 @@ for(let index = 0; index < navLinks.length; ++index)
 if(!desktopView.matches) {
     for(let service of services) {
         service.addEventListener('click', function() {
-            if(service.getAttribute('data-status') === 'reduced') {
-                service.setAttribute('data-status', 'expanded');
+            if(service.getAttribute('data-status') === dataStatus[0]) {
+                service.setAttribute('data-status', dataStatus[1]);
             } else {
-                service.setAttribute('data-status', 'reduced');
+                service.setAttribute('data-status', dataStatus[0]);
                 scrollBy({
                     behavior: 'smooth',
                     top: service.getBoundingClientRect().top
